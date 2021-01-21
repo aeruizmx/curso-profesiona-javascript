74d3a20227ac614e995fa99cc5ceeae851d3a80e
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-01-21 12:47:02 -0600

    Clase 7

--- media-player/assets/index.js
+++ media-player/assets/index.js
@@ -1,7 +1,10 @@
 import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/AutoPlay.js'
 const video = document.querySelector("video")
 
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay]});
 
-const button = document.querySelector("button")
+const button = document.querySelector("button[name='play']")
+const button2 = document.querySelector("button[name='mute']")
 button.onclick = () => player.togglePlay();
+button2.onclick = () => player.toggleMute();
