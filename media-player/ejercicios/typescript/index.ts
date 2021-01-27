3654945253b516f9df70e11b957c9ff4587d841e
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-01-27 11:51:02 -0600

    Clase 25

--- media-player/ejercicios/typescript/index.ts
+++ media-player/ejercicios/typescript/index.ts
@@ -5,3 +5,41 @@ function add(a: number,b: number){
 }
 
 const sum = add(2,3);
+
+console.log("TIPOS");
+//BOOLEAN - No se puede reasignar a otros tipos de variables
+let muted:boolean = true;
+
+//NUMEROS
+let numerador:number = 42;
+let denominador:number = 6;
+let resultado = numerador / denominador;
+
+//STRING
+let nombre: string = 'Richard'
+let saludo = `Me llamo ${nombre}`
+
+//ARREGLO
+let people: string[] = [];
+people = ["Juan", "Maria", "Jose"];
+people.push(9000); // No funciona porque el arreglo es de strings solamente
+
+let peopleAndNumbers: Array<string | number>  = []; //Arreglo de strings o numbers
+peopleAndNumbers.push("Andres")
+peopleAndNumbers.push(1);
+
+//ENUM
+enum Color {
+  Rojo = "Rojo",
+  Verde = "Verde",
+  Azul = "Azul"
+}
+let colorFav: Color = Color.Rojo;
+console.log(`Color fav es ${colorFav}`)
+
+//ANY
+let comodin:any = "Joker"
+comodin = {type:"wildcard"};
+
+//OBJECT
+let objeto:Object = {type:"wildcard"}
