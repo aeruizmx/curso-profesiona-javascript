d5470b8e7cc96238a6938870a2541e32ef15d620
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-01-30 09:40:19 -0600

    Clase 29

--- media-player/assets/index.ts
+++ media-player/assets/index.ts
@@ -1,6 +1,6 @@
-import MediaPlayer from './MediaPlayer.js'
-import AutoPlay from './plugins/AutoPlay.js'
-import AutoPause from './plugins/AutoPause.ts'
+import MediaPlayer from './MediaPlayer'
+import AutoPlay from './plugins/AutoPlay'
+import AutoPause from './plugins/AutoPause'
 const video = document.querySelector("video")
 
 const player = new MediaPlayer({ el: video, 
@@ -8,8 +8,8 @@ const player = new MediaPlayer({ el: video,
     new AutoPlay(), new AutoPause()
   ]});
 
-const button = document.querySelector("button[name='play']")
-const button2 = document.querySelector("button[name='mute']")
+const button: HTMLElement = document.querySelector("button[name='play']")
+const button2: HTMLElement = document.querySelector("button[name='mute']")
 button.onclick = () => player.togglePlay();
 button2.onclick = () => player.toggleMute();
 
