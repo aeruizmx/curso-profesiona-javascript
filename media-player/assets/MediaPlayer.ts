58ffa633cb7a9f9dd2e9b127093b2c84e67d05d9
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-02-02 14:41:36 -0600

    Clase 40

--- media-player/assets/MediaPlayer.ts
+++ media-player/assets/MediaPlayer.ts
@@ -1,12 +1,22 @@
 class MediaPlayer {
   media: HTMLMediaElement;
   plugins: Array<any>;
+  container: HTMLElement;
 
   constructor(config) {
     this.media = config.el;
     this.plugins = config.plugins || [];
+    this.initPlayer();
     this.initPlugins();
   }
+
+  initPlayer() {
+    this.container = document.createElement('div');
+    this.container.style.position = 'relative';
+    this.media.parentNode.insertBefore(this.container, this.media);
+    this.container.appendChild(this.media);
+  }
+
   private initPlugins() {
     this.plugins.forEach(plugin => {
       plugin.run(this);
@@ -32,25 +42,10 @@ class MediaPlayer {
   mute() {
     this.media.muted = true;
   }
-  
+
   unmute() {
     this.media.muted = false;
   }
-  
-  toggleMute() {
-    if (this.media.muted == true) {
-      this.unmute();
-    } else {
-      this.mute();
-    }
-  }
 }
-
-
-
-
-
-
-
 
 export default MediaPlayer;
